7dd43d9a117ca164836700c7b90cfa4a5b724301
Paul Berg <hello@paulrberg.com>
2018-10-03 17:31:55 +0100

    Added libraries

--- migrations/2_deploy_streams.js
+++ migrations/2_deploy_streams.js
@@ -4,11 +4,11 @@ const params = {
 	recipient: "0x4C50A4CF1bd11Ed1DE4A3f66171aa3906cBc2590",
 	price: "10000000000000000",
 	interval: "240",
-	duration: "17280"
+	duration: "5760"
 };
 
 module.exports = deployer => {
 	deployer.deploy(Stream, params.recipient, params.price, params.interval, params.duration, {
-		value: "720000000000000000"
+		value: "240000000000000000"
 	});
 };
